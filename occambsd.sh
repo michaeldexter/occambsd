3e1e1eaf260973cfe5f0cc0cf6a7f131860a4f0d
Michael Dexter <editor@callfortesting.org>
2023-02-25 19:17:59 -0800

    QEMU disk syntax without raw ambiguity warnings

--- occambsd.sh
+++ occambsd.sh
@@ -460,7 +460,7 @@ echo $work_dir/xen-cleanup.sh
 
 cat << HERE > $work_dir/qemu-boot.sh
 [ $( which qemu-system-aarch64 ) ] || { echo "qemu-system-aarch64 not installed" ; exit 1 ; }
-qemu-system-aarch64 -m 1024M -cpu cortex-a57 -machine virt -bios edk2-aarch64-code.fd -nographic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -rtc base=utc -hda /tmp/occambsd/vm.raw
+qemu-system-aarch64 -m 1024M -cpu cortex-a57 -machine virt -bios edk2-aarch64-code.fd -nographic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -rtc base=utc -drive file=/tmp/occambsd/vm.raw,format=raw,index=0,media=disk 
 HERE
 
 
